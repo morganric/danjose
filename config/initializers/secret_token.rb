8ff43ea2f9a47829bad3e0203e358642e1a77ad2
Richard Morgan <morganric@gmail.com>
2013-08-11 12:06:29 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Danjose::Application.config.secret_token = '53a4978a08b620b3deedbe9e33762a7e6b9ca64a749a75dd84b72d65341b8bec6880f973489c74bc274efeda04b1eb1fe417a0bc1b63b02ec51d5da782550751'
