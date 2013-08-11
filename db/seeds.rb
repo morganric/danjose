790824e2f1382352a436d3831d3cbfc2f50b8662
Richard Morgan <morganric@gmail.com>
2013-08-11 18:33:58 +0100

    pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,6 @@ end
 puts 'DEFAULT USERS'
 user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 puts 'user: ' << user.name
+puts 'password: ' << user.password
 user.add_role :admin
 user.save
