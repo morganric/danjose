60a358a635b9500548556861f8b5355210a8ba9b
Richard Morgan <morganric@gmail.com>
2013-08-11 18:16:06 +0100

    config.assets.initialize_on_precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Danjose::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  # config.assets.compile = true
   config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
