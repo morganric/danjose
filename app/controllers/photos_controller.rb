14d9495cafaf06845163c597710319400ea40ab4
Richard Morgan <morganric@gmail.com>
2013-10-29 21:36:47 +0000

    final ish

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,7 +9,7 @@ class PhotosController < ApplicationController
 
     if current_user
       if current_user.has_role? :admin
-        @photos = Photo.order('created_at DESC').page(params[:page]).per(4) 
+        @photos = Photo.order('created_at DESC').page(params[:page]).per(6) 
       end
     else
       @photos = Photo.where(:published => true).order('created_at DESC').page(params[:page]).per(4)
